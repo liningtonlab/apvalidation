0368ba90d3ea83f0154131af1f213bc66da1ada1
Ben <benledingham1@gmail.com>
2022-02-11 10:09:03 -0800

    second

--- apvalidation/__init__.py
+++ apvalidation/__init__.py
@@ -1,2 +1,4 @@
 __all__ = ['extract', 'extract_core', 'file_validation', 'request_standard', 
             'simple_file_finder', 'smiles_to_inchi', 'smiles_to_struct', 'smiles_validation']
+
+from apvalidation import extract
