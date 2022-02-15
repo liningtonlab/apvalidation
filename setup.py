fec299b7c93152dadc176cb7c0d54cbecff69678
Ben <benledingham1@gmail.com>
2022-02-15 15:16:00 -0800

    listed freq 1D

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = ["anytree>=2.8.0",
 
 setuptools.setup(
     name='apvalidation',
-    version='0.0.6',
+    version='0.0.7',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
