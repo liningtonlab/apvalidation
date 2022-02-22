474aad93170a4d7554f0dfbb64f4cab9ae174182
Jonghyeok <jka236@sfu.ca>
2022-02-22 15:23:08 -0800

    extract_duplicate

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = ["anytree==2.8.0",
 
 setuptools.setup(
     name='apvalidation',
-    version='0.1.3',
+    version='0.1.4',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
