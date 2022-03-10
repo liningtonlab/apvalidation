391ab2305375a6760939aa84a07d61e59ae4c237
Ben <benledingham1@gmail.com>
2022-03-10 23:31:08 +0000

    fixed the 2d bruker issue

--- apvalidation/extract.py
+++ apvalidation/extract.py
@@ -265,6 +265,21 @@ class Varian:
 
 class Bruker:
     """
+    ALTERATIONS TO BE MADE TO THE BRUKER FUNCTION IN ORDER TO BETTER HANDLE 2D EXPERIMENTS:
+
+    1. Change the read function so that it may take a list of filenames instead of just a single filename.
+        This is to accomodate the fact that 2D data will be analyzed by looking at both acqu and acqu2
+    
+    2. Change the find_dim function to simply look at the number of files that have been read in from the read function.
+        If there is one file then the dimension is 1D and if there are two files then the dimension is 2D.
+
+    3. Change the find_nuclei function to look at both the acqu and the acqu2 files for 2D data. For now we will only consider NUC1
+        to be the nuclei relevant to the experiment. This can be changed under further inspection.
+
+    4. Find experiment type should be the same as long as find_dim works correctly as the its just looking up the key. If find_dim 
+        works correctly and you find this is not the case then come back to here and write your findings.
+    """
+    """
     A class containing the methods to help with the extraction of
     experiment parameters from NMR data from Bruker
     """
@@ -273,7 +288,7 @@ class Bruker:
         pass
 
     @staticmethod
-    def read(filepath):
+    def read(filepath_list):
         """
         Given the raw file path to an acqu file, read the file
         and return a python dictionary with all the parameters.
@@ -281,12 +296,16 @@ class Bruker:
         :param filepath: string formatted filepath to the acqu file
         :return: dictionary containing all parameters found in the acqu file
         """
-        assert os.path.isfile(filepath)
-        param_dict = ng.bruker.read_jcamp(filename=filepath)
-        return param_dict
+        param_dict_list = []
+        for filepath in filepath_list:
+            assert os.path.isfile(filepath)
+            param_dict = ng.bruker.read_jcamp(filename=filepath)
+            param_dict_list.append(param_dict)
+        
+        return param_dict_list
 
     @staticmethod
-    def find_params(param_dict):
+    def find_params(param_dict_list):
         """
         Searches a dictionary of all the parameters from a given experiment to find specific parameters needed
         to fill the database. The parameters needed are experiment_type, nucleus 1 and 2 frequency,
@@ -295,9 +314,11 @@ class Bruker:
         :param param_dict: dictionary containing all the parameters retrieved from the file
         :return: dictionary containing only those parameters that are preferred
         """
-        exp_dim = Bruker.find_dim(param_dict)
+        param_dict = param_dict_list[0]
+
+        exp_dim = Bruker.find_dim(param_dict_list)
         exp_type = Bruker.find_exp_type(param_dict, exp_dim)
-        exp_nuc1, exp_nuc2 = Bruker.find_nuc(param_dict, exp_dim)
+        exp_nuc1, exp_nuc2 = Bruker.find_nuc(param_dict_list, exp_dim)
         exp_freq = Bruker.find_freq(param_dict, exp_dim)
         exp_solv = Bruker.find_solvent(param_dict)
         exp_temp = Bruker.find_temp(param_dict)
@@ -352,7 +373,13 @@ class Bruker:
         return exp_solv
 
     @staticmethod
-    def find_dim(param_dict):
+    def find_dim(param_dict_list):
+
+        """
+        Changes to be made to this function:
+        1. Change the argument to a list of param_dicts. If there are more than one in the list
+            then the experiment is 2D if not then it's 1D
+        """
         """
         Helper function.
         Find the dimension of the experiment given a dictionary of parameters.
@@ -361,16 +388,22 @@ class Bruker:
                             Probably returned from the read method.
         :return: dimension of the experiment.
         """
-        exp_2d_list = ['HSQCTOCSY', 'COSY', 'HSQC', 'HMQC', 'HMBC', 'TOCSY', 'ROESY', 'NOESY', 'DOSY']
-        exp_str = param_dict['EXP']
-        exp_dim = exp_str[0:2].upper()
-
-        if exp_dim not in ['1D', '2D']:
-            for entry in exp_2d_list:
-                if entry in exp_str.upper():
-                    exp_dim = '2D'
-                    return exp_dim
+        exp_dim = None
+        if len(param_dict_list) == 2:
+            exp_dim = '2D'
+        elif len(param_dict_list) == 1:
             exp_dim = '1D'
+
+        # exp_2d_list = ['HSQCTOCSY', 'COSY', 'HSQC', 'HMQC', 'HMBC', 'TOCSY', 'ROESY', 'NOESY', 'DOSY']
+        # exp_str = param_dict['EXP']
+        # exp_dim = exp_str[0:2].upper()
+
+        # if exp_dim not in ['1D', '2D']:
+        #     for entry in exp_2d_list:
+        #         if entry in exp_str.upper():
+        #             exp_dim = '2D'
+        #             return exp_dim
+        #     exp_dim = '1D'
         return exp_dim
 
     @staticmethod
@@ -383,20 +416,23 @@ class Bruker:
         :param exp_dim: dimension of the experiment
         :return: type of experiment in string. (1D experiments are not given a type)
         """
-        exp_str = param_dict['EXP']
+        possible_exp_str_1 = param_dict['EXP']
+        possible_exp_str_2 = param_dict['PULPROG']
+
         exp_2d_list = ['HSQCTOCSY', 'COSY', 'HSQC', 'HMQC', 'HMBC', 'TOCSY', 'ROESY', 'NOESY', 'DOSY']
         # assume exp_type is 1D and change from there 
         exp_type = ''
         if exp_dim == '2D':
             for entry in exp_2d_list:
-                if entry in exp_str:
+                if entry in possible_exp_str_1.upper() or entry in possible_exp_str_2.upper():
                     exp_type = entry
                     return exp_type
             exp_type = None
             return exp_type
         else:
+            print("This experiment is 1D!")
             for entry in exp_2d_list:
-                if entry in exp_str:
+                if entry in possible_exp_str_1 or entry in possible_exp_str_2:
                     exp_type = entry
                     break
             exp_type = f'1D-{exp_type}'
@@ -424,7 +460,7 @@ class Bruker:
             return freq_val
 
     @staticmethod
-    def find_nuc(param_dict, exp_dim):
+    def find_nuc(param_dict_list, exp_dim):
         """
         Helper function.
         Determine the nuclei that are used in the experiment.
@@ -434,16 +470,31 @@ class Bruker:
         :param exp_dim: the dimension of the experiment
         :return: nucleus 1 and nucleus 2 in string format
         """
+        param_dict_1D = param_dict_list[0]
+        if exp_dim == '2D':
+            param_dict_2D = param_dict_list[1]
+
         exp_nuc2 = None
         if exp_dim == '1D':
-            exp_nuc1 = param_dict['NUC1']
-        elif param_dict['NUC2'] != 'off':
-            exp_nuc1 = param_dict['NUC1']
-            exp_nuc2 = param_dict['NUC2']
+            exp_nuc1 = param_dict_1D['NUC1']
+        elif exp_dim == '2D':
+            exp_nuc1 = param_dict_1D['NUC1']
+            exp_nuc2 = param_dict_2D['NUC1']
         else:
-            exp_nuc1 = param_dict['NUC1']
-            exp_nuc2 = param_dict['NUC1']
+            exp_nuc1 = None
+
         return exp_nuc1, exp_nuc2
+        
+        # exp_nuc2 = None
+        # if exp_dim == '1D':
+        #     exp_nuc1 = param_dict['NUC1']
+        # elif param_dict['NUC2'] != 'off':
+        #     exp_nuc1 = param_dict['NUC1']
+        #     exp_nuc2 = param_dict['NUC2']
+        # else:
+        #     exp_nuc1 = param_dict['NUC1']
+        #     exp_nuc2 = param_dict['NUC1']
+        # return exp_nuc1, exp_nuc2
 
 
 class Jcampdx:
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Assert
 from apvalidation.smiles_validation import validate_struct
 from apvalidation import smiles_to_inchikey
 from apvalidation.extract import Varian, Bruker, Jcampdx
@@ -12,15 +13,36 @@ import numpy as np
 #     output = Varian.find_params(varian_dict)
 #     print(output)
 
-dict, data = ng.varian.read_fid("./test_files/Lobosamide C/Lobos9512D_A_2_gHMBCAD_DMSO_600MHz_5mmShigemi.fid/fid 2")
-data_1D = np.array(data)
-print(data_1D)
-print(data_1D.shape)
+# dict, data = ng.varian.read_fid("./test_files/Lobosamide C/Lobos9512D_A_2_gHMBCAD_DMSO_600MHz_5mmShigemi.fid/fid 2")
+# data_1D = np.array(data)
+# print(data_1D)
+# print(data_1D.shape)
 
-dict, data = ng.varian.read_fid("./test_files/Lobosamide C/Lobos9512D_A_2_gHMBCAD_DMSO_600MHz_5mmShigemi.fid/fid")
-data_2D = np.array(data)
-print(data_2D)
-print(data_2D.shape)
+# dict, data = ng.varian.read_fid("./test_files/Lobosamide C/Lobos9512D_A_2_gHMBCAD_DMSO_600MHz_5mmShigemi.fid/fid")
+# data_2D = np.array(data)
+# print(data_2D)
+# print(data_2D.shape)
 
-check = data_1D==data_2D
-print(False in check)
+# check = data_1D==data_2D
+# print(False in check)
+for filename in os.listdir("./test_files/Granaticin_D"):
+    print(filename)
+    if os.path.exists(f"test_files/Granaticin_D/{filename}/acqu2"):
+        file_list = [f"test_files/Granaticin_D/{filename}/acqu", f"test_files/Granaticin_D/{filename}/acqu2"]
+    else:
+        file_list = [f"test_files/Granaticin_D/{filename}/acqu"]
+    dict_list = Bruker.read(file_list)
+    params = Bruker.find_params(dict_list)
+    print(params)
+
+print("\n2nd folder ---------------------------------------------------------------------------------------\n")
+
+for filename in os.listdir("./test_files/Demethoxy-cornuside"):
+    print(filename)
+    if os.path.exists(f"test_files/Demethoxy-cornuside/{filename}/acqu2"):
+        file_list = [f"test_files/Demethoxy-cornuside/{filename}/acqu", f"test_files/Demethoxy-cornuside/{filename}/acqu2"]
+    else:
+        file_list = [f"test_files/Demethoxy-cornuside/{filename}/acqu"]
+    dict_list = Bruker.read(file_list)
+    params = Bruker.find_params(dict_list)
+    print(params)
