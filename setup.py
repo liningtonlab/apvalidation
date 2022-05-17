6b6f9ca2fcbf05840f13f1a919ebec8e5dff4981
Ben Ledingham <benledingham1@gmail.com>
2022-05-17 18:51:58 +0000

    change to version 3.42

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setuptools.setup(
     name='apvalidation',
-    version='0.3.41',
+    version='0.3.42',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
