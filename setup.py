e55380722cce22fefbfe4143055730cc157a5e2c
Ben Ledingham <benledingham1@gmail.com>
2022-05-17 18:50:15 +0000

    increment to version 3.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setuptools.setup(
     name='apvalidation',
-    version='0.3.03',
+    version='0.3.1',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
