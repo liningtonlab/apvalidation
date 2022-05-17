8f994b4cef78846c0015dda419fc6bcb520c0c1e
Ben Ledingham <benledingham1@gmail.com>
2022-05-17 18:46:09 +0000

    docs strings for peak list added

--- apvalidation/peak_validator.py
+++ apvalidation/peak_validator.py
@@ -8,7 +8,7 @@ from rdkit.Chem import rdqueries, rdmolops
 
 
 """
-Create three classes, one to validate Proton peaks and one to validate Carbon peaks
+These are custom exception classes to help annotate what error we are handling.
 """
 
 class InvalidCharacters(Exception):
@@ -33,7 +33,12 @@ class Validate:
 
     @staticmethod
     def check_valid_characters(text_block):
+        """
+            Check that only accepted characters are present in the text taken from the input text box. 
 
+            :param text_block: The text taken from the user input
+            :return: If valid, return a string confirming valid chars. If invalid raise InvalidCharacters exception.
+        """
         clean_text = text_block.replace(" ", "")
         accepted_pattern = re.compile(r"^[\d . - ( ) , ; \u002D \u05BE \u1806 \u2010 \u2011 \u2012 \u2013 \\\
                                          \u2014 \u2015 \u207B \u208B \u2212 \uFE58 \uFE63 \uFF0D]+$", re.UNICODE)
@@ -44,6 +49,14 @@ class Validate:
     
     @staticmethod
     def parse_text_to_list(valid_text):
+        """
+            Split the text from the peak list text box into a list of values and ranges. There are multiple
+            different list formats to try.
+
+            :param valid_text: The text to split
+            :return: If works, return the split list. If split does not work, raise a NoSplit exception.
+        """
+
         try:
             split_text = valid_text.split(",")
             if len(split_text) == 1:
@@ -69,10 +82,17 @@ class Validate:
 
     @staticmethod
     def is_valid_range(value):
+        """
+            Check if the value is of a valid range format. The format is specified by the regex expression where the unicode characters
+            are different representations of dashes.
+
+            :param value: a value from the peak list that may be a valid range
+            :return: True or False depending on if the value is a valid range.
+        """
         value = value.replace(" ", "")
         range_pattern = re.compile(r"^\({0,1}\d{1,3}.{0,1}\d{0,4} {0,5}[\u002D\u05BE\u1806\u2010\u2011\u2012\u2013 \\\
                                     \u2014\u2015\u207B\u208B\u2212\uFE58\uFE63\uFF0D]{1} {0,5}\d{1,3}.{0,1}\d{0,4}\){0,1}$", re.UNICODE)
-        re_result = re.search(range_pattern, value)         
+        re_result = re.search(range_pattern, value)
         if re_result:
             return True
         else:
@@ -80,26 +100,41 @@ class Validate:
 
     @staticmethod
     def check_data_type(value_list):
+        """
+            Check the datatype for each value in the peak value list. Ensure that the type is either 
+            a float value or a properly formatted range of values, ex. (13.2 - 14.5).
 
+            :param value_list: a list of the values and ranges in the peak list.
+            :return: A list of booleans which represents which values in value_list are valid datatypes and which are not.
+                    Ex. If all are valid return [True, True,...], If first is invalid and rest are valid return [False, True, True, ...]
+
+        """
         value_check_list = [False]*len(value_list)
         value_list = [value.replace(" ","") for value in value_list]
 
         for index, value in enumerate(value_list):
             if Validate.is_valid_range(value):
                 value_check_list[index] = True
-                print(value_check_list)
-                return value_check_list
             else:
                 try:
                     if isinstance(float(value.replace(" ", "")), float):
                         value_check_list[index] = True
-                        print(value_check_list)
-                        return value_check_list
+
                 except ValueError:
-                    return value_check_list
+                    continue
+        return value_check_list
 
     @staticmethod
     def check_number_atoms(value_list, atom_type, smiles):
+        """
+            Check that the number of atoms in the list is between 0 and the amount of atoms in the
+            molecule structure provided by the smiles string.
+
+            :param value_list: a list of the values and ranges in the peak list.
+            :param atom_type: the type of atom being checked either H or C.
+            :param smiles: the smiles string for the corresponding compound. 
+            :return: If input is valid, return True. If input is invalid, raise InvalidAtomNumer exception.
+        """
         atoms = {"H": 1, "C": 6}
         atom_num = atoms[atom_type]
         mol = Chem.MolFromSmiles(smiles)
@@ -115,7 +150,13 @@ class Validate:
 
     @staticmethod
     def check_value_ranges(value_list, atom_type):
+        """
+            Ensure the numerical range for each value makes sense for the type of atom. Ex. H > -2 etc..
 
+            :param value_list: a list of the values and ranges in the peak list.
+            :param atom_type: the type of atom being checked either H or C
+            :return: If valid input, return True. If invalid input, return error message.
+        """
         unchecked_values = []
         for value in value_list:
             is_range = Validate.is_valid_range(value)
@@ -148,6 +189,16 @@ class Validate:
 
     @staticmethod
     def validate(H_text_block, C_text_block, smiles):
+        """
+            Check that the peak lists given check some basic validity checks before accepting them into the DB.
+
+            :param H_text_block: The text entered into the H peak list text box.
+            :param C_text_block: The text entered into the C peak list text box.
+            :param smiles: The smiles string for the corresponding compound.
+            :return: If the input is valid, return a string confirming its validity. If the input is not valid, 
+                    raise an Exception specifying the problem with the input.
+        """
+
         # Check that characters in the text block are valid 
         try:
             Validate.check_valid_characters(H_text_block)
@@ -179,11 +230,13 @@ class Validate:
                     else:
                         continue
                 raise InvalidValueType
-
         except InvalidValueType:
             return f"Error: The following values are of invalid data type {error_value_list}"
+        
+        # Check the datatype for each of the entries in each list
         try:
-            if sum(Validate.check_data_type(C_list)) != len(H_list):
+            error_list = Validate.check_data_type(C_list)
+            if sum(error_list) != len(C_list):
                 error_value_list = []
                 for index, b_val in enumerate(H_list):
                     if b_val is False:
@@ -194,6 +247,7 @@ class Validate:
         except InvalidValueType:
             return f"Error: The following values are of invalid data type {error_value_list}"
 
+        # Check the number of atoms in each list do not exceed amount of atoms in struct
         try:
             Validate.check_number_atoms(H_list, "H", smiles)
         except InvalidAtomNumber:
@@ -203,6 +257,7 @@ class Validate:
         except InvalidAtomNumber:
             return "Error: Invalid number of C atoms in the peak list"
 
+        # Check the values to ensure they are real H or C values
         try:
             output = Validate.check_value_ranges(H_list, "H")
         except ErrorBadRange:
@@ -214,141 +269,3 @@ class Validate:
 
         return "Both lists are valid"
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Validate:
-#     """
-#     This will be the class that contains the validate_peak_lists function.
-#     This function will be the one called by the user in order to validate the peaks.
-#     """
-#     @staticmethod
-#     def check_lengths(peak_dict):
-#         h_peaks = peak_dict['H']
-#         c_peaks = peak_dict['C']
-#         if len(h_peaks) != len(c_peaks):
-#             return False
-#         return True
-
-#     @staticmethod
-#     def validate_peak_lists(peak_dict, smiles):
-
-#         return_bool = True
-#         return_messages = []
-
-#         if Validate.check_lengths(peak_dict) is False:
-#             return_bool = False
-#             return_messages.append("Length of carbon and proton list do not match.")
-
-#         # currently returns the number of unique protons doesn't work properly I need to know what to check
-#         num_protons = Proton.check_atom_counts(smiles)
-#         print(num_protons)
-#         if num_protons is False:
-#             return_bool = False
-#             return_messages.append("Invalid number of unique protons.")
-
-#         # currently returns the number of carbons doesn't work properly I need to know what to check
-#         num_carbons = Carbon.check_atom_counts(smiles)
-#         print(num_carbons)
-#         if num_carbons is False:
-#             return_bool = False
-#             return_messages.append("Invalid number of unique carbons.")  
-
-#         h_list = peak_dict['H']
-#         if Proton.check_value_ranges(h_list) is False:
-#             return_messages.append("WARNING: Proton values out of range.")
-
-#         c_list = peak_dict['C']
-#         if Carbon.check_values_ranges(c_list) is False:
-#             return_messages.append("WARNING: Carbon values out of range.")
-
-#         return return_bool, return_messages
-
-# class Proton:
-
-#     @staticmethod
-#     def check_value_ranges(h_list):
-#         mean = stats.mean(h_list)
-#         median = stats.median(h_list)
-#         if mean > 10:
-#             return False
-#         if median > 10:
-#             return False
-        
-#         for item in h_list:
-#             if item > 20:
-#                 return False
-#         return True
-
-#     def check_atom_counts(smiles):
-#         mol = Chem.MolFromSmiles(smiles)
-#         mol_h = Chem.AddHs(mol)
-#         ordered_atoms = Chem.CanonicalRankAtoms(mol_h, breakTies=False)
-#         unique_hs = set()
-#         all_hs = []
-#         for atom, sym_classes in zip(mol_h.GetAtoms(), ordered_atoms):
-#             if atom.GetAtomicNum() == 1:
-#                 all_hs.append(atom)
-#                 unique_hs.add(sym_classes)
-
-#         return len(unique_hs)
-
-
-# class Carbon:
-
-#     @staticmethod
-#     def check_values_ranges(c_list):
-#         mean = stats.mean(c_list)
-#         median = stats.median(c_list)
-#         if mean < 10:
-#             return False
-#         if median < 10:
-#             return False
-        
-#         for item in c_list:
-#             if item > 250:
-#                 return False
-#         return True
-
-#     @staticmethod
-#     def check_atom_counts(smiles):
-#         mol = Chem.MolFromSmiles(smiles)
-#         all_cs = []
-#         for atom in mol.GetAtoms():
-#             if atom.GetAtomicNum() == 6:
-#                 all_cs.append(atom)
-
-#         return len(all_cs)
-
-
-
-    
