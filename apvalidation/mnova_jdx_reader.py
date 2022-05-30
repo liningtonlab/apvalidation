abe1ba3829df6807baa93ad79a470745bd276b93
Ben Ledingham <benledingham1@gmail.com>
2022-05-30 20:50:32 +0000

    docstrings

--- apvalidation/mnova_jdx_reader.py
+++ apvalidation/mnova_jdx_reader.py
@@ -33,8 +33,8 @@ def find_deep_groups(line_level_list, max_depth):
         be a list of groupings where each group is the largest set contiguous lines at the two deepest
         levels of the file. The purpose of this is to remove the meta data tagging that comes with the combined
         jdx file.
-        :param line_level_list: a list of dict objects that contain the value for each line as well as the depth the line is
-                                nested at.
+        :param line_level_list: a list of dict objects that contain the value for each line as well as the
+                                depth the line is nested at.
         :param max_depth: an int representing the max nesting in the file
         :return: a list containing groupings of deep lines in the file. This list is separated by lines that were
                 contiguous in the original file.
@@ -55,22 +55,32 @@ def find_deep_groups(line_level_list, max_depth):
         
     return groups
 
-def make_filename(item, group, group_num):
+def make_filename(item, group_num):
     """
         determine the name of the single experiment jdx file being saved.
-        :param item: a single line 
+        :param item: a single line in the group being checked
+        :param group_num: the number of the group being saved
+        :return: the title of the file or None if not found yet
     """
 
-    for item in group:
-        if item["value"].startswith("##TITLE="):
-            group_title = item["value"].split("=")[1]
-            group_title = group_title.split("/")[-1]
-            group_title = f"{group_title}_exp_{group_num+1}.jdx"
-            return group_title
-        else:
-            return "titlenotfound"
+    if item["value"].startswith("##TITLE="):
+        group_title = item["value"].split("=")[1]
+        group_title = group_title.split("/")[-1]
+        group_title = f"{group_title}_exp_{group_num+1}.jdx"
+        return group_title
+    else:
+        return None
+
 
 def save_separate_files(merged_groups, save_path):
+    """
+        save separate jdx files for each experiment found in the input file. 
+        These files are saved into the folder path provided.
+        :param merged_groups: a list of the groups, each element of the list 
+                                should be saved as its own jdx file
+        :param save_path: a filepath pointing to the folder to save the files to.
+        :return: the folder path which the files were saved to
+    """
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
@@ -88,7 +98,10 @@ def save_separate_files(merged_groups, save_path):
 
     for index, group in enumerate(merged_groups):
         for item in group:
-            group_title = make_filename(item, group, index)
+            group_title = make_filename(item, index)
+            if group_title != None:
+                break
+
         
         single_file = open(f"{save_path}/{group_title}", "w+")
         for index, item in enumerate(group):
@@ -99,6 +112,13 @@ def save_separate_files(merged_groups, save_path):
 
     
 def separate_mnova_jdx(input_filepath, save_location):
+    """
+        use the above functions to fully separate a combined jdx file and save
+        it as a folder of separate files (one per experiment)
+        :param input_filepath: the filepath to the combined jdx file
+        :param save_location: the filepath to the folder that the single jdx are saved to
+        :return: a path to the saved folder
+    """
     with open(f"{input_filepath}", "r") as f:
         line_list = f.readlines()
         line_list_tabs = add_indents(line_list)
