939b0da54b5f360092fe434754c72257f47169e8
Matthew Pin <MattTPin@gmail.com>
2022-08-26 19:30:45 +0000

    increment version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setuptools.setup(
     name='apvalidation',
-    version='0.4.87',
+    version='0.4.88',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
