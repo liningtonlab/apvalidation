b207bc61b1e43764e5bc17602c791b9a28dc7f24
Matthew Pin <MattTPin@gmail.com>
2022-11-04 16:30:28 +0000

    changed "CDCL3" to "CDCl3"

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setuptools.setup(
     name='apvalidation',
-    version='0.5.13',
+    version='0.5.14',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
