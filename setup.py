b37ab445f4a512405bfe488723516350b216fa81
Matthew Pin <MattTPin@gmail.com>
2023-06-16 20:42:58 +0000

    updated version num

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setuptools.setup(
     name='apvalidation',
-    version='0.5.41',
+    version='0.5.42',
     author='liningtonlabs',
     author_email='liningtonlabstest@gmail.com',
     description='Testing installation of Package',
