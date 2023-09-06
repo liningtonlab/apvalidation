de70c7661406e72dbafad96f573b2ff8096ab209
Matthew Pin <MattTPin@gmail.com>
2023-09-06 22:09:21 +0000

    added data_files to setup

--- apvalidation/extract_bruker.py
+++ apvalidation/extract_bruker.py
@@ -3,10 +3,10 @@ import nmrglue as ng
 import json
 from collections import OrderedDict
 
-with open('apvalidation/experiment_standardizer.json', 'r') as file:
+with open('apvalidation/metadata_standardizers/experiment_standardizer.json', 'r') as file:
     exp_dict = json.load(file) 
     
-with open('apvalidation/solvent_standardizer.json', 'r') as file:
+with open('apvalidation/metadata_standardizers/solvent_standardizer.json', 'r') as file:
     all_solvents = json.load(file)
     
     
--- apvalidation/extract_varian.py
+++ apvalidation/extract_varian.py
@@ -7,10 +7,10 @@ import sys
 import shutil
 import json
 
-with open('apvalidation/experiment_standardizer.json', 'r') as file:
+with open('apvalidation/metadata_standardizers/experiment_standardizer.json', 'r') as file:
     exp_dict = json.load(file) 
     
-with open('apvalidation/solvent_standardizer.json', 'r') as file:
+with open('apvalidation/metadata_standardizers/solvent_standardizer.json', 'r') as file:
     all_solvents = json.load(file)
     
 
