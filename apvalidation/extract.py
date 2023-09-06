16b0b581498ccd8dc244c3a7f4886e9293996694
Matthew Pin <MattTPin@gmail.com>
2023-09-06 21:32:31 +0000

    sdf

--- apvalidation/extract.py
+++ apvalidation/extract.py
@@ -1,6 +1,6 @@
-from .extract_classes.varian import Varian
-from .extract_classes.bruker import Bruker
-from .extract_classes.jeol import JEOL
-from .extract_classes.jcampdx import Jcampdx
+from apvalidation.extract_classes.varian import Varian
+from apvalidation.extract_classes.bruker import Bruker
+from apvalidation.extract_classes.jeol import JEOL
+from apvalidation.extract_classes.jcampdx import Jcampdx
 
 __all__ = ["Varian", "Bruker", "JEOL", "Jcampdx"]
