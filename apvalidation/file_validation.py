9172ca1f58117cd09592f52457d66ab499dfacb5
Matthew Pin <MattTPin@gmail.com>
2023-10-19 21:43:43 +0000

    removed whitespace strip from filename

--- apvalidation/file_validation.py
+++ apvalidation/file_validation.py
@@ -210,7 +210,7 @@ def add_path_vendor(path, params, vendor_type, filetype, res_dict):
     file_root_without_file_name = str(path)
     if file_root_without_file_name == ".":
         file_root_without_file_name = "/"
-    params["original_data_path"] = file_root_without_file_name.strip()
+    params["original_data_path"] = file_root_without_file_name
     params["vendor"] = vendor_type
     params["filetype"] = filetype
     res_dict.append(params)
