96724f693357e0e730a96230fcd83dda11f6dc5e
Matthew Pin <MattTPin@gmail.com>
2025-06-17 19:42:21 +0000

    increment version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setuptools.setup(
     name="apvalidation",
-    version="0.5.76",
+    version="0.5.77",
     author="liningtonlabs",
     author_email="liningtonlabstest@gmail.com",
     description="Testing installation of Package",
